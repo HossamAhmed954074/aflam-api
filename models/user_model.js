2ec875c24b0f825693b0ebabbe9462b44cf58ce6
Hossam Ahmed <ha954074@gmail.com>
2025-11-17 15:02:16 +0200

    feat: include user type in JWT token generation for enhanced user identification

--- models/user_model.js
+++ models/user_model.js
@@ -16,12 +16,17 @@ const userSchema = new db.Schema({
     },
   },
   password: { type: String, required: true },
+  type: {
+    type: String,
+    default: "Basic",
+    enum: ["Admin", "Basic"],
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
 userSchema.method("genAuthToken", function () {
   const token = jwt.sign(
-    { id: this._id, email: this.email, name: this.name },
+    { id: this._id, email: this.email, name: this.name, type: this.type },
     process.env.JWT_SECRET,
     {
       expiresIn: process.env.JWT_EXPIRES_IN,
